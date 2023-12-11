d4fd76b98e198b07c89139d800549c5f2249801e
FelipeGinklings <felipe.ginklings@gmail.com>
2023-12-11 17:30:18 -0300

    Exploring Layouts & Flexbox & Commenting old code

--- App.js
+++ App.js
@@ -1,28 +1,44 @@
+import { TextInput } from 'react-native';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<View>
-				<Text style={styles.dummyText}>Another piece of text!</Text>
+		// Old code for the initial classes
+		// <View style={styles.container}>
+		// 	<View>
+		// 		<Text style={styles.dummyText}>Another piece of text!</Text>
+		// 	</View>
+		// 	<Text style={styles.dummyText}>Hello World!</Text>
+		// 	<Button title="Tap Me!" />
+		// </View>
+		<View style={styles.appContainer}>
+			<View style>
+				<TextInput placeholder="Your course goal!" />
 			</View>
-			<Text style={styles.dummyText}>Hello World!</Text>
-			<Button title="Tap Me!" />
+			<Button title="Add Goal" />
+			<View>
+				<Text>List of goals...</Text>
+			</View>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	dummyText: {
-		margin: 16,
-		borderWidth: 1,
-		borderColor: 'red',
-		padding: 16,
+	// Old code for the initial classes
+	// container: {
+	// 	flex: 1,
+	// 	backgroundColor: '#fff',
+	// 	alignItems: 'center',
+	// 	justifyContent: 'center',
+	// },
+	// dummyText: {
+	// 	margin: 16,
+	// 	borderWidth: 1,
+	// 	borderColor: 'red',
+	// 	padding: 16,
+	// },
+
+	appContainer: {
+		padding: 50,
 	},
 });
