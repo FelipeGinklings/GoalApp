66d1900b0050c11d9d5b0d6a4a0c2aa3ee1ccc9d
FelipeGinklings <felipe.ginklings@gmail.com>
2023-12-13 11:01:36 -0300

    Splitting Components Into Smaller Components

---
+++ components/GoalInput.js
@@ -0,0 +1,12 @@
+// import { StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
+
+// const GoalInput = () => {
+//   return (
+
+//   )
+// }
+
+// export default GoalInput
+
+// const styles = StyleSheet.create({})
