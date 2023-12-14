c656b656d4710482c9239718640610d9d093c6f3
FelipeGinklings <felipe.ginklings@gmail.com>
2023-12-14 17:46:58 -0300

    Making Items Deletable & Using IDs

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,11 +2,11 @@ import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 const GoalItem = (props) => {
 	return (
-    <Pressable onPress={props.onDeleteItem}>
-      <View style={styles.goalItem}>
-        <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+		<Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
+			<View style={styles.goalItem}>
+				<Text style={styles.goalText}>{props.text}</Text>
+			</View>
+		</Pressable>
 	);
 };
 
