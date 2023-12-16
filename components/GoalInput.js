3da5c4e3a7d06dd19011bb3f41465b8b727c7a44
Felipe Ginklings <felipe.ginklings@gmail.com>
2023-12-15 23:31:49 -0300

    Adding a Modal Screen

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, View, Button, TextInput } from 'react-native';
+import { StyleSheet, View, Button, TextInput, Modal } from 'react-native';
 
 const GoalInput = (props) => {
 	const [enteredGoalText, setEnteredGoalText] = useState(['']);
@@ -13,15 +13,21 @@ const GoalInput = (props) => {
 		setEnteredGoalText('');
 	};
 	return (
-		<View style={styles.inputContainer}>
-			<TextInput
-				style={styles.textInput}
-				placeholder="Your course goal!"
-				onChangeText={goalInputHandler}
-        value={enteredGoalText}
-			/>
-			<Button color="#5e0acc" title="Add Goal" onPress={addGoalHandler} />
-		</View>
+		<Modal visible={props.visible} animationType='slide'>
+			<View style={styles.inputContainer}>
+				<TextInput
+					style={styles.textInput}
+					placeholder="Your course goal!"
+					onChangeText={goalInputHandler}
+					value={enteredGoalText}
+				/>
+				<Button
+					color="#5e0acc"
+					title="Add Goal"
+					onPress={addGoalHandler}
+				/>
+			</View>
+		</Modal>
 	);
 };
 
